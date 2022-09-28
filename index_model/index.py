4a355d4920508c1b2208d5048f79328aa8cc60e0
linwei <linweiwu@pku.edu.cn>
2022-09-28 11:36:52 -0400

    Read original data

--- index_model/index.py
+++ index_model/index.py
@@ -3,9 +3,8 @@ import pandas as pd
 import numpy as np
 
 class IndexModel:
-    def __init__(self) -> None:
-        # To be implemented
-        pass
+    def __init__(self):
+        self.stock_prices = pd.read_csv('./data_sources/stock_prices.csv')
 
     def calc_index_level(self, start_date: dt.date, end_date: dt.date) -> None:
         # To be implemented

7fe97b0bfe683b1cc06d07746645f7cf67f83fe6
linwei <linweiwu@pku.edu.cn>
2022-09-28 11:38:03 -0400

    get stock prices

--- index_model/index.py
+++ index_model/index.py
@@ -6,9 +6,9 @@ class IndexModel:
     def __init__(self):
         self.stock_prices = pd.read_csv('./data_sources/stock_prices.csv')
 
-    def calc_index_level(self, start_date: dt.date, end_date: dt.date) -> None:
-        # To be implemented
-        pass
+    def calc_index_level(self, start_date: dt.date, end_date: dt.date) :
+        date = self.stock_prices["Date"]
+        stock_prices = self.stock_prices
 
     def export_values(self, file_name: str) -> None:
         # To be implemented

43cdf4e1b390d6e59d3127f54f63d83d3339ed52
linwei <linweiwu@pku.edu.cn>
2022-09-28 11:40:49 -0400

    Get the top three stocks at the beginning of each month

--- index_model/index.py
+++ index_model/index.py
@@ -19,6 +19,35 @@ class IndexModel:
             if t.date() == end_date:
                 end_index = i
         
+        '''
+        Get the top three stocks
+        '''
+        max1 = 0
+        max2 = 0
+        max3 = 0
+        s1 = "N/A"
+        s2 = "N/A"
+        s3 = "N/A"
+        stock1 = []
+        stock2 = []
+        stock3 = []
+        for i in range (0, len(date)):
+            t = dt.datetime.strptime(date[i],'%d/%m/%Y')
+            if i == 0 :
+                month = t.month
+            if t.month != month:
+                day_price = stock_prices.iloc[i-1][1:stock_prices.shape[1]]
+                max1 = sorted(day_price.tolist())[-1]
+                max2 = sorted(day_price.tolist())[-2]
+                max3 = sorted(day_price.tolist())[-3]
+                s1 = stock_prices.iloc[i-1][stock_prices.iloc[i-1]==max1].index.tolist()[0]
+                s2 = stock_prices.iloc[i-1][stock_prices.iloc[i-1]==max2].index.tolist()[0]
+                s3 = stock_prices.iloc[i-1][stock_prices.iloc[i-1]==max3].index.tolist()[0]
+            stock1.append(s1)
+            stock2.append(s2)
+            stock3.append(s3)
+            month = t.month
+            
         
 
     def export_values(self, file_name: str) -> None:

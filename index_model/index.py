dd91df206d6583352460432dff5e2b3a9e2545d4
linwei <linweiwu@pku.edu.cn>
2022-09-28 11:39:05 -0400

    Mark start date and end date

--- index_model/index.py
+++ index_model/index.py
@@ -9,6 +9,17 @@ class IndexModel:
     def calc_index_level(self, start_date: dt.date, end_date: dt.date) :
         date = self.stock_prices["Date"]
         stock_prices = self.stock_prices
+        
+        start_index = 0
+        end_index = 0
+        for i in range (0, len(date)):
+            t = dt.datetime.strptime(date[i],'%d/%m/%Y')
+            if t.date() == start_date:
+                start_index = i
+            if t.date() == end_date:
+                end_index = i
+        
+        
 
     def export_values(self, file_name: str) -> None:
         # To be implemented
